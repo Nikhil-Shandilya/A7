9ff7fcbfcab743e80eb55ba4fb49a2a1d6d24ed0
Nikhil-Shandilya <149268205+Nikhil-Shandilya@users.noreply.github.com>
2026-01-15 12:54:59 +0530

    Update clean.py

--- clean.py
+++ clean.py
@@ -4,3 +4,15 @@ df = pd.read_csv("data.csv")
 df["marks"].fillna(df["marks"].mean(), inplace=True)
 df.to_csv("data_clean.csv", index=False)
 
+"""
+sudo snap install dvc -classic
+dvc --version
+git init
+dvc init
+dvc add data.csv
+git add .
+git commit -m "addded"
+dvc repro
+dvc status
+cat clean_data.csv
+"""
